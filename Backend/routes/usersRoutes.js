2f5ba7c5b67328c57175c5f5baa84bb7d901cc91
Acil <acilhamieh4@gmail.com>
2025-03-17 15:52:40 +0200

    add userroute

--- Backend/routes/usersRoutes.js
+++ Backend/routes/usersRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
+const {getAllUsers,getUserById,deleteUserById,createUser}=require('../controllers/usersController'); 
 
-
+router.get('/allusers', getAllUsers);
+router.get('/user/:id',getUserById);
+router.post('/adduser',createUser);
+router.delete('/deleteuser/:id',deleteUserById);
 
 export default router;
