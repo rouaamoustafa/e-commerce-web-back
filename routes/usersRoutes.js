a2d876317c2d504c7e8b073782d9ba1e3ee65bd3
Acil <acilhamieh4@gmail.com>
2025-03-20 09:55:37 +0200

    add is admin , verifytoken functions

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { getAllUsers, getUserById, deleteUserById, createUser,loginUser } from '../controllers/usersController.js'; 
-
+import { isAdmin,verifyToken } from '../controllers/authController.js';
 const router = express.Router();
 
-router.get('/allusers', getAllUsers);//done
-router.get('/user/:id', getUserById);//done
+router.get('/allusers',  isAdmin,verifyToken,getAllUsers);//done1done
+router.get('/user/:id', isAdmin,verifyToken,getUserById);//donedone
 router.post('/adduser', createUser);//done
-router.delete('/deleteuser/:id', deleteUserById);//done
+router.delete('/deleteuser/:id', isAdmin,verifyToken, deleteUserById);//donedone
 router.post("/login", loginUser);//done
 
 export default router;
